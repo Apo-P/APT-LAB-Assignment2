f681d071b9f6966f217634e83befee6a9e2bab01
Apo-P <tolara2000@gmail.com>
2022-05-13 16:24:47 +0300

    Added new product

--- src/main/java/gr/upatras/rest/example/ProductService.java
+++ src/main/java/gr/upatras/rest/example/ProductService.java
@@ -27,6 +27,7 @@ public class ProductService implements IProductService {
 		products.add(new Product(103, "Laptop", "LHP29OCP", 24000.00, 1));
 		products.add(new Product(104, "Air Conditioner", "ACLG66721", 30000.00, 5));
 		products.add(new Product(105, "Refrigerator ", "12WP9087", 10000.00, 4));
+		products.add(new Product(106, "Prebuilt Computer", "42LMAO12", 1000.00, 3));
 	}
 	
 	/**
